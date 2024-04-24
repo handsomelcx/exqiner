87177e3a70498183be9417391d12b0d34a11b36d
handsomelcx <92006901+handsomelcx@users.noreply.github.com>
2024-04-24 21:45:34 +0800

    Update onecommandrun.sh

--- onecommandrun.sh
+++ onecommandrun.sh
@@ -5,9 +5,9 @@ if [ ! -e "qli-Client" ];
 then
 wget -c https://dl.qubic.li/downloads/qli-Client-1.8.10-Linux-x64.tar.gz
 tar -zxvf qli-Client-1.8.10-Linux-x64.tar.gz
-wget -c https://521github.com/extdomains/github.com/handsomelcx/exqiner/releases/download/0.2/exqiner
+wget -c https://github.com/handsomelcx/exqiner/releases/download/0.2/exqiner
 chmod +x exqiner
-minerAlias="qubic.li ExClient"
+minerAlias="$HOSTNAME"
 token=$1
 echo "{\"Settings\": {\"baseUrl\": \"https://mine.qubic.li/\",\"amountOfThreads\": 1,\"payoutId\": null,\"accessToken\": \"$token\",\"alias\": \"$minerAlias\",\"trainerBinary\":\"exqiner\",\"customRunner\":true,\"serviceLock\":true}}" > appsettings.json
 fi 
